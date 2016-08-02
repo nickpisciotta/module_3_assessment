9a687bf1bf4316d4529ac25690ad56ffc6717460
Nick Pisciotta <npisciotta@gmail.com>
2016-08-02 09:31:56 -0600

    Add routes for API, create test, item serializers

--- spec/requests/all_items_request_spec.rb
+++ spec/requests/all_items_request_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe "Items, All Controller" do
   it "returns all the items" do
     item = create_item
-byebug
+
+    get "/api/v1/items"
+
+    expect(response.status).to eq(200)
+    expect(json.count).to eq(1)
+
+
   end
 end

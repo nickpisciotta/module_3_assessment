bf1d0e8ec3a6ea580b6a8580ac07cbe1c4202d7d
Nick Pisciotta <npisciotta@gmail.com>
2016-08-02 09:46:45 -0600

    Add show action to api items controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -5,5 +5,8 @@ class Api::V1::ItemsController < ApplicationController
     render :json => Item.all
   end
 
+  def show
+    render :json => Item.find(params[:id])
+  end
 
 end
--- spec/requests/all_items_request_spec.rb
+++ spec/requests/all_items_request_spec.rb
@@ -1,14 +1,35 @@
 require 'rails_helper'
 
-describe "Items, All Controller" do
+describe "Items Controller" do
   it "returns all the items" do
     item = create_item
+    item2 = create_item
 
     get "/api/v1/items"
 
     expect(response.status).to eq(200)
-    expect(json.count).to eq(1)
 
+    parsed_response = JSON.parse(response.body)
 
+    expect(parsed_response.count).to eq(2)
+    expect(parsed_response.first["name"]).to eq(item.name)
+    expect(parsed_response.first["description"]).to eq(item.description)
+    expect(parsed_response.first["image_url"]).to eq(item.image_url)
+  end
+
+  it "returns one item" do
+    item = create_item
+    item2 = create_item
+
+    get "/api/v1/items/1"
+
+    expect(reponse.status).to eq(200)
+
+    parsed_response = JSON.parse(response.body)
+
+    expect(parsed_response.count).to eq(1)
+    expect(parsed_response["name"]).to eq(item.name)
+    expect(parsed_response["description"]).to eq(item.description)
+    expect(parsed_response["image_url"]).to eq(item.image_url)
   end
 end

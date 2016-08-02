29970ad68c2662c7c4f31786ea93258978b683ac
Nick Pisciotta <npisciotta@gmail.com>
2016-08-02 12:02:34 -0600

    Couldn't get stores to show in view

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,18 +1,21 @@
 class Store
 
-  def initialize(zip_code)
-    @_zip_code = zip_code
-  end
-
   def self.service
-    @service ||= StoreService.new(@zip_code)
+     @service ||= StoreService.new
   end
 
   def self.search_stores_by_zip_code(zip_code)
     store_search_response = service.get_stores_by_zip_code(zip_code)
-    # store_search = store_search_response
-    byebug
   end
+
+  def total_stores
+    Store.search_store_by_zip_code(zip_code)["total"]
+  end
+
+  def stores
+    Store.search_store_by_zip_code(zip_code)["stores"]
+  end 
+
 
   private
     def zip_code

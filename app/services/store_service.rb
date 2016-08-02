b949344c0832b8d375bf4c1d9fd0b909d701e60f
Nick Pisciotta <npisciotta@gmail.com>
2016-08-02 11:16:38 -0600

    First connection successful with service

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -1,12 +1,25 @@
 class StoreService
 
   def initialize
-    @_connection = Faraday.connect
-    @_connection.params["apiKey"] = ENV["best_buy_api_key"]
+    @_connection = Faraday.new("https://api.bestbuy.com/v1/")
+    @_connection.params["apiKey"]   = ENV["best_buy_api_key"]
+    @_connection.params["format"]   = "json"
+    @_connection.params["show"]     = "storeId,storeType,name"
+    @_connection.params["pageSize"] = 2
   end
 
   def get_stores_by_zip_code(zip_code)
-
+    response = connection.get("stores(area(#{zip_code},25))")
+    parse(response)
+    byebug
   end
 
+  private
+    def connection
+      @_connection
+    end
+
+    def parse(response)
+      JSON.parse(response.body)
+    end
 end
--- spec/models/store_service_spec.rb
+++ spec/models/store_service_spec.rb
@@ -1,3 +1,14 @@
 require 'rails_helper'
 
-RSpec
+RSpec.describe StoreService do
+  context "#get_stores_by_zip_code" do
+  it "returns all stores" do
+    VCR.use_cassette("total_steps") do
+      zip_code = "80202"
+      connection = StoreService.new
+      response = connection.get_stores_by_zip_code(zip_code)
+
+      end
+    end
+  end
+end

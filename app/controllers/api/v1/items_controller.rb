ad5097e6f7f3ead19fc9f4fcd0ddf6fc77a8dfb6
Nick Pisciotta <npisciotta@gmail.com>
2016-08-02 10:46:39 -0600

    Add search controller and route

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -18,10 +18,9 @@ class Api::V1::ItemsController < ApplicationController
   def create
     @item = Item.create(item_params)
     if @item.save
-      render json: {@item, status: 201}
+      render :json => @item, :status => :created
     end
   end
-
 
   private
     def item_params
